6ac3d90560bcfeff611afd46985fa6591aadebc9
Lozano-Fernandez <77489969+Lozano-Fernandez@users.noreply.github.com>
2024-07-03 15:06:24 +0200

    Update site_concordance.sh

--- Maximum_Likelihood.JESUS_LOZANO/Bear_dataset_ML/Dissecting_signal/site_concordance.sh
+++ Maximum_Likelihood.JESUS_LOZANO/Bear_dataset_ML/Dissecting_signal/site_concordance.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-iqtree -t ../ultrafast_bootstrap/50_genes.fa.treefile -s 50_genes.fa -m MODEL --scfl 100 --prefix site_concordance -nt 8
+iqtree -t ../ultrafast_bootstrap/50_genes.fa.treefile -s 50_genes.fa -m MODEL --scfl 100 --prefix site_concordance -nt 8 -redo
