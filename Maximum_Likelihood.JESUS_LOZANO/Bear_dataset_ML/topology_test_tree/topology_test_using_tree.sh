360f2551ad47ed16b97ddf82c5f7a28050f33ffb
Lozano-Fernandez <77489969+Lozano-Fernandez@users.noreply.github.com>
2024-07-03 13:56:27 +0200

    Update topology_test_using_tree.sh

--- Maximum_Likelihood.JESUS_LOZANO/Bear_dataset_ML/topology_test_tree/topology_test_using_tree.sh
+++ Maximum_Likelihood.JESUS_LOZANO/Bear_dataset_ML/topology_test_tree/topology_test_using_tree.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-iqtree -s 50_genes.fa -z ../bear_species_trees_topologies.tre -m MODEL -te ../ultrafast_bootstrap/50_genes.fa.treefile -zb 1000 -zw -au nt 8 -redo
+iqtree -s 50_genes.fa -z ../bear_species_trees_topologies.tre -m MODEL -te ../ultrafast_bootstrap/50_genes.fa.treefile -zb 1000 -zw -au -nt 8 -redo
